0a413f66cddf2812ce447d4d7620883999b686db
S.Botchorishvili <S.Botchorishvili@tbcpay.ge>
2023-04-21 14:25:25 +0400

    removed preloader and made new one in index.html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Ripple } from "react-preloaders2";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Ripple color="#fff" background="#86c232" />
     <App />
   </React.StrictMode>
 );
